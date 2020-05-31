e0efe91f0581f23d8b0dd90fc0834c083ca82da5
JereChen <jerechen11@gmail.com>
2020-05-31 10:54:11 +0800

    migrate project to androidX

--- app/src/main/java/com/example/androiddesignpattern/factorypattern/factorymethodpattern/FactoryMethodTestActivity.java
+++ app/src/main/java/com/example/androiddesignpattern/factorypattern/factorymethodpattern/FactoryMethodTestActivity.java
@@ -1,6 +1,6 @@
 package com.example.androiddesignpattern.factorypattern.factorymethodpattern;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
--- app/src/main/java/com/example/androiddesignpattern/observerpattern/functionone/FunctionOneActivity.java
+++ app/src/main/java/com/example/androiddesignpattern/observerpattern/functionone/FunctionOneActivity.java
@@ -1,7 +1,7 @@
 package com.example.androiddesignpattern.observerpattern.functionone;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.androiddesignpattern.R;
